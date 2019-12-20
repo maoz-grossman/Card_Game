44ac59ba5830686e6da4c26df839e23eba9227d9
maoz-grossman <maozg2008@gmail.com>
2019-12-20 13:12:05 +0200

    Deck,Prospeetor&Card script were added

---
+++ Card_Game/Assets/__Scripts/Card.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    //this class will be defined later
+}
+
+[System.Serializable]
+/* A Serializable class is able to be 
+ edited in the Inspector*/
+public class Decorator
+{
+    //this class srotes information about each decorator or pip from DeckXML
+    public string type; //for card pips, tye="pip"
+    public Vector3 loc;//the location of the Sprite on the Card
+    public bool flip = false;//whether to flip the Sprite vertically
+    public float scale = 1f;//the scale of the Sprite
+}
+
+[System.Serializable]
+public class CardDefinition
+{
+    //this class stores information for each rank of card
+    public string face;//sprite to use for each card
+    public int rank;//the rank(1-13) of thus card
+    public List<Decorator> pips = new List<Decorator>();//pips used
+}
---
+++ Card_Game/Assets/__Scripts/Deck.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    [Header("Set Dynamically")]
+    public PT_XMLReader xmlr;
+    //InitDeck is called by prospector wwhen it is ready
+    public void InitDeck(string deckXMLText)
+    {
+        ReadDeck(deckXMLText);
+    }
+    //ReadDeck parses the XML file passed to it into CardDefinition
+    public void ReadDeck(string deckXMLText)
+    {
+        xmlr = new PT_XMLReader();//create a new PT_XMLReder 
+        xmlr.Parse(deckXMLText);
+        //this prints a test line to show you how xmlr can be used
+        string s = "xml[0] decorator[0]";
+        s += "type" + xmlr.xml["xml"][0]["decorator"][0].att("type");
+        s += " x=" + xmlr.xml["xml"][0]["decorator"][0].att("x");
+        s += " y=" + xmlr.xml["xml"][0]["decorator"][0].att("y");
+        s += " scale=" + xmlr.xml["xml"][0]["decorator"][0].att("scales");
+        print(s);
+    }
+}
