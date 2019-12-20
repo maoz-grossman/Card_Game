5d86a1637fbcb8dd7e3b9c9f7a5aa69c6961bd45
maoz-grossman <maozg2008@gmail.com>
2019-12-20 13:28:46 +0200

    prospector was added

--- Card_Game/Assets/__Scripts/Prospector.cs
+++ Card_Game/Assets/__Scripts/Prospector.cs
@@ -6,15 +6,19 @@ using UnityEngine.UI; //will be used later
 
 public class Prospector : MonoBehaviour
 {
-    // Start is called before the first frame update
+    static public Prospector S;
+    [Header("Set in inspector")]
+    public TextAsset deckXML;
+    [Header("Set Dynamically")]
+    public Deck deck;
+
+    void Awake()
+    {
+        S = this;//set up a singleton for prospector    
+    }
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        deck = GetComponent<Deck>();// get the Deck
+        deck.InitDeck(deckXML.text);// pass DeckXML to it
     }
 }
