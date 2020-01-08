3286e44ad4a9d9a20e50a3128bf58f692dee8ede
maoz-grossman <maozg2008@gmail.com>
2020-01-08 18:18:04 +0200

    Change Card class

--- Prospector Solitaire/Assets/__Scripts/Card.cs
+++ Prospector Solitaire/Assets/__Scripts/Card.cs
@@ -16,7 +16,68 @@ public class Card : MonoBehaviour
     public List<GameObject> pipGOs = new List<GameObject>();
     public GameObject back;//the GameObject of the back of the card
     public CardDefinition def;//parsed from DeckXML.xml
-    
+    //List of the SpriteRenderer Components of this GameObject
+    public SpriteRenderer[] spriteRenderers;
+
+     void Start()
+    {
+        SetSortOrder(0);
+    }
+
+    //If spriteRenderers is not yet define, this function defins it 
+    public void PopulateSpriteRenderers()
+    {
+        // If spriteRenderers is null or empty
+        if(spriteRenderers==null||spriteRenderers.Length==0)
+        {
+            //Get SpriteRenderer Components of this GameObject and its children
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+    //Sets the sortingLayerName on all SpriteRenderer Components
+    public void SetSortingLayerName(string tSLN)
+    {
+        PopulateSpriteRenderers();
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
+
+    //Sets the sortingOreder of all SpriteRenderer Components
+    public void SetSortOrder(int sOrd)
+    {
+        PopulateSpriteRenderers();
+
+        //Iterate through all the spriteRenderers as tSR
+        foreach(SpriteRenderer tSR in spriteRenderers)
+        {
+            //If the gmeObject is this.gameObject,it's the backgroud
+            if (tSR.gameObject == this.gameObject)
+            {
+                tSR.sortingOrder = sOrd;//Set it's order to sOrd
+                continue;//And continue to the next iteration of the loop
+            }
+           //Each of the children of this GameObject are named
+           //switch based on the names
+           switch(tSR.gameObject.name)
+            {
+                case "back":
+                    //Set it to the highest layer to cover the other sprites
+                    tSR.sortingOrder = sOrd + 2;
+                    break;
+                case "face"://if the name is "face"
+                default:// or if it's anything else
+                    //Set it to the middle layer to be above the background
+                    tSR.sortingOrder = sOrd + 1;
+                    break;
+            }
+        }
+    }
+
+
+
     public bool faceUp
     {
         get
